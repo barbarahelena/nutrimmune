6a2f0f0f6ca6b70c58da8c629613dadce0f9ba90
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2025-01-29 19:50:08 +0100

    remove some 2 year plots (too low N)

--- correlations.R
+++ correlations.R
@@ -93,12 +93,12 @@ ggsave("results/correlations/bmiv1v5_diet.pdf", width = 10, height = 20)
 plist <- c()
 for(a in names(baria)[4:18]) {
     baria$var <- baria[[a]]
-    pl <- ggplot(data = baria, aes(x = log10(var), y = bmiperc_v1v5)) +
+    pl <- ggplot(data = baria, aes(x = log10(var), y = bmiperc_v1v4)) +
         geom_point(alpha = 0.7, color = ggsci::pal_bmj()(1)) +
         geom_smooth(method = "lm") +
         stat_cor() +
         theme_Publication() +
-        labs(title = paste0(a), y = "bmi % change at 2 years",
+        labs(title = paste0(a), y = "bmi % change at 1 year",
              x = paste0(a))
     plist[[a]] <- pl
 }
@@ -137,20 +137,20 @@ for(a in names(baria)[4:18]) {
 ggarrange(plotlist = plist, ncol = 3, nrow = 5, labels = LETTERS[1:15])
 ggsave("results/correlations/homairv4_diet.pdf", width = 10, height = 20)
 
-plist <- c()
-for(a in names(baria)[4:18]) {
-    baria$var <- baria[[a]]
-    pl <- ggplot(data = baria, aes(x = log10(var), y = log10(homaIR_v5+0.01))) +
-        geom_point(alpha = 0.7, color = ggsci::pal_bmj()(1)) +
-        geom_smooth(method = "lm") +
-        stat_cor() +
-        theme_Publication() +
-        labs(title = paste0(a), y = "HOMA-IR at 2 years",
-             x = paste0(a))
-    plist[[a]] <- pl
-}
-ggarrange(plotlist = plist, ncol = 3, nrow = 5, labels = LETTERS[1:15])
-ggsave("results/correlations/homairv5_diet.pdf", width = 10, height = 20)
+# plist <- c()
+# for(a in names(baria)[4:18]) {
+#     baria$var <- baria[[a]]
+#     pl <- ggplot(data = baria, aes(x = log10(var), y = log10(homaIR_v5+0.01))) +
+#         geom_point(alpha = 0.7, color = ggsci::pal_bmj()(1)) +
+#         geom_smooth(method = "lm") +
+#         stat_cor() +
+#         theme_Publication() +
+#         labs(title = paste0(a), y = "HOMA-IR at 2 years",
+#              x = paste0(a))
+#     plist[[a]] <- pl
+# }
+# ggarrange(plotlist = plist, ncol = 3, nrow = 5, labels = LETTERS[1:15])
+# ggsave("results/correlations/homairv5_diet.pdf", width = 10, height = 20)
 
 plist <- c()
 for(a in names(baria)[4:18]) {
@@ -169,19 +169,19 @@ ggarrange(plotlist = plist, ncol = 3, nrow = 5, labels = LETTERS[1:15])
 ggsave("results/correlations/homairv1v4_diet.pdf", width = 10, height = 20)
 
 
-plist <- c()
-for(a in names(baria)[4:18]) {
-    baria$var <- baria[[a]]
-    pl <- ggplot(data = baria, aes(x = log10(var), y = homaIR_v1v5*-1)) +
-        scale_y_log10() +
-        geom_point(alpha = 0.7, color = ggsci::pal_bmj()(1)) +
-        geom_smooth(method = "lm") +
-        stat_cor() +
-        theme_Publication() +
-        labs(title = paste0(a), y = "Change HOMA-IR at 1 year",
-             x = paste0(a))
-    plist[[a]] <- pl
-}
-ggarrange(plotlist = plist, ncol = 3, nrow = 5, labels = LETTERS[1:15])
-ggsave("results/correlations/homairv1v5_diet.pdf", width = 10, height = 20)
+# plist <- c()
+# for(a in names(baria)[4:18]) {
+#     baria$var <- baria[[a]]
+#     pl <- ggplot(data = baria, aes(x = log10(var), y = homaIR_v1v5*-1)) +
+#         scale_y_log10() +
+#         geom_point(alpha = 0.7, color = ggsci::pal_bmj()(1)) +
+#         geom_smooth(method = "lm") +
+#         stat_cor() +
+#         theme_Publication() +
+#         labs(title = paste0(a), y = "Change HOMA-IR at 1 year",
+#              x = paste0(a))
+#     plist[[a]] <- pl
+# }
+# ggarrange(plotlist = plist, ncol = 3, nrow = 5, labels = LETTERS[1:15])
+# ggsave("results/correlations/homairv1v5_diet.pdf", width = 10, height = 20)
 
